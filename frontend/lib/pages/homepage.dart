4dcd0945b06f9b461559293412204348512f64e9
RYzeOnGit <ryan.chattopadhyay1@gmail.com>
2025-01-09 04:33:26 -0700

    pages folder and themes

---
+++ frontend/lib/pages/homepage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
