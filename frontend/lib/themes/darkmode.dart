478340359ecb4252a025f9a9a89e9476447d653d
RYzeOnGit <ryan.chattopadhyay1@gmail.com>
2025-01-09 05:03:06 -0700

    dark mode bug fix

--- frontend/lib/themes/darkmode.dart
+++ frontend/lib/themes/darkmode.dart
@@ -5,7 +5,7 @@ ThemeData darkmode = ThemeData(
     surface: Colors.grey.shade800,
     primary: Colors.grey.shade700,
     secondary: Colors.grey.shade900,
-    tertiary: Colors.black
+    tertiary: Colors.black,
     inversePrimary: Colors.grey.shade300,
   ),
 );
