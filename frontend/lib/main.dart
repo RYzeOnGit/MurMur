4f58fb7e21a3659346394b1ef2f1491169c4fc2d
Mayank Khandelwal <imayankhandelwal@gmail.com>
2025-01-11 12:31:57 -0700

    Basic Changes in the main

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -2,13 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'pages/homepage.dart';
 import 'themes/themeprovider.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
-Future <void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+void main() {
   runApp(ChangeNotifierProvider(
     create: (context) => Themeprovider(),
     child: const MyApp(),
