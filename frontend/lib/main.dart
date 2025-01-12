edd1ff4fc76bbef247d846e88136cef0e2eb9b9d
tridha643 <tridhatriv@gmail.com>
2025-01-11 19:38:32 -0700

    some erros occurring trying to debug

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -2,25 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'pages/homepage.dart';
 import 'themes/themeprovider.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-
-Future <void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(ChangeNotifierProvider(
-    create: (context) => Themeprovider(),
-    child: const MyApp(),
-  ));
+ 
+void main() {
+  runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false ,
+      debugShowCheckedModeBanner: false,
       home: Homepage(),
       theme: Provider.of<Themeprovider>(context).themeData,
     );
