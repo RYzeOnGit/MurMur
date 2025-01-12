0081f70c218d91b015b6e08dac9745333425e976
Mayank Khandelwal <imayankhandelwal@gmail.com>
2025-01-12 05:23:33 -0700

    Initialized the AppHomePage.dart, will have to work on it

---
+++ frontend/lib/pages/AppHomePage.dart
@@ -0,0 +1,12 @@
+import "package:flutter/material.dart";
+
+class Apphomepage {
+  const Apphomepage();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
